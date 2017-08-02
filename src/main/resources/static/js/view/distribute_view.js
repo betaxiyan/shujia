fcb4c7421690e375718c442860f3f286b7d595aa
yuanpeng <yuanpeng@bonc.com.cn>
2017-08-02 13:40:33 +0800

    feat：新增了租户信息、已经划配等js文件

---
+++ src/main/resources/static/js/view/distribute_view.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+/*    $('#view_tabs a').click(function(e) {
+            e.preventDefault()
+            $(this).tab('show')
+        });
+*/
+   $('#distribute_table').dataTable({
+        "language": {
+            "paginate": {
+                "previous": "首页",
+                "next": "下一页"
+            },
+            "info": "显示_START_到_END_, 共计_TOTAL_条数据",
+            "emptyTable": "无记录",
+            "infoEmpty": "共计0",
+        },
+        /* 分页设置 */
+        "bPaginate": true,
+        "bLengthChange": false,
+        /* 搜索设置 */
+        "bFilter": true,
+        "bSort": false,
+        /* 显示总条数 */
+        "bInfo": true,
+        "bAutoWidth": false
+    });
+})
