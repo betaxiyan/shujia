46007be16118b313c12d641949f2e462f5a6a1e4
SunZhiyong <sunzhiyong@bonc.com.cn>
2017-08-03 15:59:59 +0800

    feat:租户资源中间表数据导入数据库

---
+++ src/main/java/com/bonc/nerv/tioa/entity/TenantResourceMidEntity.java
@@ -0,0 +1,281 @@
+/*
+ * 文件名：TenantResourceMidEntity.java
+ * 版权：Copyright by www.bonc.com.cn
+ * 描述：
+ * 修改人：zhiyong
+ * 修改时间：2017年8月2日
+ */
+
+package com.bonc.nerv.tioa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *  租户资源中间表实体类
+ * @author zhiyong
+ * @version 2017年8月2日
+ * @see TenantResourceMidEntity
+ * @since
+ */
+
+@Entity
+@Table(name = "tenant_resource_mid")
+public class TenantResourceMidEntity {
+    
+    /**
+     * 表id
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "trm_id")
+    private Long trmId;
+    
+    /**
+     * 租户id
+     */
+    @Column(name = "tenant_id")
+    private String tenantId;
+    
+    /**
+     * 租户名
+     */
+    @Column(name = "tenant_name")
+    private String tenantName;
+    
+    /**
+     * 服务类型
+     */
+    @Column(name = "service_type")
+    private String serviceType;
+    
+    /**
+     * 租户账号
+     */
+    @Column(name = "tenant_account")
+    private String tenantAccount;
+    
+    /**
+     * ip地址
+     */
+    @Column(name = "ip_addr")
+    private String ipAddr;
+    
+    /**
+     * 资源类型
+     */
+    @Column(name = "type_name")
+    private String typeName;
+    
+    /**
+     * 存储使用量
+     */
+    @Column(name = "storage")
+    private String storage;
+    
+    /**
+     * cpu 核数
+     */
+    @Column(name = "cpu_num")
+    private String cpuNum;
+    
+    /**
+     * 内存大小
+     */
+    @Column(name = "memory")
+    private String memory;
+    
+    /**
+     * 服务名
+     */
+    @Column(name = "service_name")
+    private String serviceName;
+    
+    /**
+     * 队列名
+     */
+    @Column(name = "sequence_name")
+    private String sequenceName;
+    
+    /**
+     * 机房
+     */
+    @Column(name = "compute_room")
+    private String computeRoom;
+    
+    /**
+     * 申请日期
+     */
+    @Column(name = "ask_date")
+    private String askDate;
+    
+    /**
+     * 开放日期
+     */
+    @Column(name = "open_date")
+    private String openDate;
+    
+    /**
+     * 更新时间(原为退租时间)
+     */
+    @Column(name = "end_rent_data")
+    private String endRentData;
+    
+    /**
+     * 是否有效
+     */
+    private String isInvalid;
+    
+    public Long getTrmId() {
+        return trmId;
+    }
+
+    public void setTrmId(Long trmId) {
+        this.trmId = trmId;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getTenantName() {
+        return tenantName;
+    }
+
+    public void setTenantName(String tenantName) {
+        this.tenantName = tenantName;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public String getTenantAccount() {
+        return tenantAccount;
+    }
+
+    public void setTenantAccount(String tenantAccount) {
+        this.tenantAccount = tenantAccount;
+    }
+
+    public String getIpAddr() {
+        return ipAddr;
+    }
+
+    public void setIpAddr(String ipAddr) {
+        this.ipAddr = ipAddr;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getStorage() {
+        return storage;
+    }
+
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+
+    public String getCpuNum() {
+        return cpuNum;
+    }
+
+    public void setCpuNum(String cpuNum) {
+        this.cpuNum = cpuNum;
+    }
+
+    public String getMemory() {
+        return memory;
+    }
+
+    public void setMemory(String memory) {
+        this.memory = memory;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getSequenceName() {
+        return sequenceName;
+    }
+
+    public void setSequenceName(String sequenceName) {
+        this.sequenceName = sequenceName;
+    }
+
+    public String getComputeRoom() {
+        return computeRoom;
+    }
+
+    public void setComputeRoom(String computeRoom) {
+        this.computeRoom = computeRoom;
+    }
+
+    public String getAskDate() {
+        return askDate;
+    }
+
+    public void setAskDate(String askDate) {
+        this.askDate = askDate;
+    }
+
+    public String getOpenDate() {
+        return openDate;
+    }
+
+    public void setOpenDate(String openDate) {
+        this.openDate = openDate;
+    }
+
+    public String getEndRentData() {
+        return endRentData;
+    }
+
+    public void setEndRentData(String endRentData) {
+        this.endRentData = endRentData;
+    }
+    
+    
+    
+    public String getIsInvalid() {
+        return isInvalid;
+    }
+
+    public void setIsInvalid(String isInvalid) {
+        this.isInvalid = isInvalid;
+    }
+
+    @Override
+    public String toString() {
+        return "TenantResourceMidEntity [trmId=" + trmId + ", tenantId=" + tenantId
+               + ", tenantName=" + tenantName + ", serviceType=" + serviceType + ", tenantAccount="
+               + tenantAccount + ", ipAddr=" + ipAddr + ", typeName=" + typeName + ", storage="
+               + storage + ", cpuNum=" + cpuNum + ", memory=" + memory + ", serviceName="
+               + serviceName + ", sequenceName=" + sequenceName + ", computeRoom=" + computeRoom
+               + ", askDate=" + askDate + ", openDate=" + openDate + ", endRentData=" + endRentData
+               + ", isInvalid=" + isInvalid + "]";
+    }
+}
