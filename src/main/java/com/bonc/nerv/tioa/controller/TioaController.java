5b03703476abd81a1aa2af1ac3025e4ece9a97d5
SunZhiyong <sunzhiyong@bonc.com.cn>
2017-08-04 09:39:51 +0800

    fix:创建ExternalRestController

--- src/main/java/com/bonc/nerv/tioa/controller/TioaController.java
+++ src/main/java/com/bonc/nerv/tioa/controller/TioaController.java
@@ -3,9 +3,7 @@ package com.bonc.nerv.tioa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.bonc.nerv.tioa.service.ExternalRestService;
 import com.bonc.nerv.tioa.service.TioaService;
 
 
@@ -18,14 +16,7 @@ import com.bonc.nerv.tioa.service.TioaService;
  * @since
  */
 @Controller
-public class TioaController {
-    
-    /**
-     * 声明调用外部restful接口的服务类
-     */
-    @Autowired
-    private ExternalRestService externalRestService;
-    
+public class TioaController {    
     @Autowired
     private TioaService tioaService;
     
@@ -52,32 +43,6 @@ public class TioaController {
     public String inter2(){
         return "fragment/charging_frg";
     }
-    
-    
-    /**
-     * 
-     * 调用接口获取资源和账号数据写入数据库中
-     * @see
-     */
-    @RequestMapping("/resAccToDb")
-    @ResponseBody
-    public void resAccToDb(){
-        externalRestService.resAccToDb();
-    }
-    
-    /**
-     * 
-     * Description: <br>
-     * 调用接口获取账号和文件数数据写入数据库中
-     * @param sysDate 时间参数
-     * @see
-     */
-    @RequestMapping("/accFcountToDb")
-    @ResponseBody
-    public void accFcountToDb(@RequestParam("sysDate") String sysDate) {
-        externalRestService.accFcountToDb(sysDate);
-    }
-    
     
     @RequestMapping("/savaToFile")
     @ResponseBody
