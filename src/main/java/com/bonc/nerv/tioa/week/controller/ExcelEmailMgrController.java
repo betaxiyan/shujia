a43e17a2db89f14aac8b5fab55c4323ea5e8899d
SunZhiyong <sunzhiyong@bonc.com.cn>
2017-08-08 17:09:42 +0800

    feat:实现excel文件合并功能

--- src/main/java/com/bonc/nerv/tioa/week/controller/ExcelEmailMgrController.java
+++ src/main/java/com/bonc/nerv/tioa/week/controller/ExcelEmailMgrController.java
@@ -8,14 +8,49 @@
 
 package com.bonc.nerv.tioa.week.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 /**
- * Excel和邮件控制器
- * @author yuanpeng
- * @version 2017年8月6日
- * @see ExcelEmailManagerController
+ * 
+ * Excel及邮箱控制类
+ * @author zhiyong
+ * @version 2017年8月8日
+ * @see ExcelEmailMgrController
  * @since
  */
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.bonc.nerv.tioa.week.service.MergeExcelService;
+/**
+ * 
+ * 导出Excel
+ * @author zhiyong
+ * @version 2017年8月8日
+ * @see ExcelEmailMgrController
+ * @since
+ */
+@Controller
 public class ExcelEmailMgrController {
-
+    
+    /**
+     * MergeExcelService
+     */
+    @Autowired
+    private MergeExcelService mergeExcelService;
+    
+    /**
+     * 导出Excel
+     * @param response HttpServletResponse
+     * @return 操作提示信息
+     * @see
+     */
+    @RequestMapping("/getExcel")
+    @ResponseBody
+    public String getExcel(HttpServletResponse response) {
+        mergeExcelService.getExcel(response);
+        return "导出Excel成功！";
+    }
 }
