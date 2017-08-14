4f85a0d79eef5f1264137d681b5456640568fca3
hcn <huangchunningyx@163.com>
2017-08-14 12:44:48 +0800

    feat: 屏蔽刷新数据表数据的查询功能

--- src/main/resources/static/js/view/health_view.js
+++ src/main/resources/static/js/view/health_view.js
@@ -1,138 +1,165 @@
-
 $(document).ready(function() {
-    $("#test_health").click(restfulHealth);
-    $("#update").click(updateTable);
+	$("#test_health").click(restfulHealth);
+	$("#update").click(updateTable);
 });
 
-function restfulHealth(){
+function restfulHealth() {
 
-		   $.ajax({
-		       type : "GET",
-		       url : ctx+"restfulHealth",
-		       dataType:"json",
-		       contentType: "application/x-www-form-urlencoded; charset=utf-8",
-		       success : function(data) {
-		    	   	var data = data.data;
-		    	   	console.log(data);
-		            var length = data.length;
-		            //每次测试都要刷新表中数据
-		            $('#detail_health tbody').html("");
-		            for(var i=0;i<length;i++)
-		                {
-		                  $('#detail_health tbody').append("<tr>"+
-		                        "<td>"+data[i].restfulName+"</td>" +
-		                        "<td>"+data[i].restfulURL+"</td>" +
-		                        "<td>"+data[i].status+"</td>" +
-		                        "<td>"+data[i].checkDate+"</td>" +
-		                                "</tr>");
-		                }
+	$.ajax({
+		type : "GET",
+		url : ctx + "restfulHealth",
+		dataType : "json",
+		contentType : "application/x-www-form-urlencoded; charset=utf-8",
+		success : function(data) {
+			var data = data.data;
+			console.log(data);
+			var length = data.length;
+			// 每次测试都要刷新表中数据
+			$('#detail_health tbody').html("");
+			for (var i = 0; i < length; i++) {
+				$('#detail_health tbody').append(
+						"<tr>" + "<td>" + data[i].restfulName + "</td>"
+								+ "<td>" + data[i].restfulURL + "</td>"
+								+ "<td>" + data[i].status + "</td>" + "<td>"
+								+ data[i].checkDate + "</td>" + "</tr>");
+			}
 
-		       }
-		   }); 
-		   
-   }
+		}
+	});
+
+}
 $('#detail_health').dataTable({
-	
-    "language": {
-        "paginate": {
-            "previous": "首页",
-            "next": "下一页"
-        },
-        "info": "显示_START_到_END_, 共计_TOTAL_条数据",
-        "emptyTable": "无记录",
-        "infoEmpty": "共计0",
-    },
-   //  分页设置 
-    "bPaginate": true,
-    "bLengthChange": false,
-   //  搜索设置 
-    "bFilter": true,
-    "bSort": false,
-   //  显示总条数 
-    "bInfo": true,
-    "bAutoWidth": false,
-    
-    "ajax":{ url: ctx+"restfulHealth",},
-    		
-    "columns": [ 
-                   //mData 表示发请求时候本列的列明，返回的数据中相同下标名字的数据会填充到这一列
-                   {"mData": 'restfulName'},
-                   {"mData": 'restfulURL'},
-                   {"mData": 'status'},
-                   {"mData": 'checkDate'},
 
-               ],
+	"language" : {
+		"paginate" : {
+			"sFirst": "首页",
+            "sPrevious": "前一页",
+            "sNext": "后一页",
+            "sLast": "尾页"
+		},
+		"info" : "显示_START_到_END_, 共计_TOTAL_条数据",
+		"emptyTable" : "无记录",
+		"infoEmpty" : "共计0",
+		"sSearch" : "查询：",
+		"sZeroRecords" : "查询不到任何相关数据",
+	},
+	// 分页设置
+	"bPaginate" : true,
+	"bLengthChange" : false,
+	// 搜索设置
+	"bFilter" : true,
+	"bSort" : false,
+	// 显示总条数
+	"bInfo" : true,
+	"bAutoWidth" : false,
+
+	"ajax" : {
+		url : ctx + "restfulHealth",
+	},
+	"columns" : [
+	// mData 表示发请求时候本列的列明，返回的数据中相同下标名字的数据会填充到这一列
+	{
+		"mData" : 'restfulName'
+	}, {
+		"mData" : 'restfulURL'
+	}, {
+		"mData" : 'status'
+	}, {
+		"mData" : 'checkDate'
+	},
+
+	],
 });
 
 $('#upTableData').dataTable({
-	
-    "language": {
-        "paginate": {
-            "previous": "首页",
-            "next": "下一页"
-        },
-        "info": "显示_START_到_END_, 共计_TOTAL_条数据",
-        "emptyTable": "无记录",
-        "infoEmpty": "共计0",
-    },
-   //  分页设置 
-    "bPaginate": true,
-    "bLengthChange": false,
-   //  搜索设置 
-    "bFilter": true,
-    "bSort": false,
-   //  显示总条数 
-    "bInfo": true,
-    "bAutoWidth": false,
+
+	"language" : {
+		"paginate" : {
+			"sFirst": "首页",
+            "sPrevious": "前一页",
+            "sNext": "后一页",
+            "sLast": "尾页"
+		},
+		"info" : "显示_START_到_END_, 共计_TOTAL_条数据",
+		"emptyTable" : "无记录",
+		"infoEmpty" : "共计0",
+		"sSearch" : "查询：",
+		"sZeroRecords" : "查询不到任何相关数据",
+		"lengthMenu": "每页显示 _MENU_ 记录",
+		"processing": "加载中......",
+	},
+	// 分页设置
+	"bPaginate" : true,
+	"bLengthChange" : false,
+	//"aLengthMenu":[[1,2,3,5],[1,2,3,5]],
+	// 搜索设置,无法指定数据源
+	"bFilter" : false,
+	"bSort" : false,
+	// 显示总条数
+	"bInfo" : true,
+	"bAutoWidth" : false,
+	"destroy" : true,
+
+	/*"ajax" : {
+		url : ctx +,
+	},*/
+	"columns" : [
+	{
+		"mData" : 'tableName'
+	}, {
+		"mData" : 'updateDate'
+	}, {
+		"mData" : 'result'
+	},
+	],
 });
 
-function updateTable(){
+function updateTable() {
 
 	$('.updateresult').text("更新中…");
-	//刷新Ttl数据
-	   $.ajax({
-	       type : "GET",
-	       url : ctx+"getMidDateToTtl",
-	       dataType:"json",
-	       contentType: "application/x-www-form-urlencoded; charset=utf-8",
-	       success : function(data) {
-	    	   	var data = data.data;
-	    	   	console.log(data);
-	            var length = data.length;
-	            //每次刷新表中数据,注意给更新结果那一列加上class属性
-	            $('#Ttl').html("");
-	            for(var i=0;i<length;i++)
-	                {
-	                  $('#Ttl').append(
-	                        "<td>"+data[i].tableName+"</td>" +
-	                        "<td>"+data[i].updateDate+"</td>" +
-	                        "<td class=\"updateresult\">"+data[i].result+"</td>");
-	                }
-	       }
-	   }); 
-	   
-	   //刷新Ttd数据
-	   $.ajax({
-	       type : "GET",
-	       url : ctx+"getMidDateToTtd",
-	       dataType:"json",
-	       contentType: "application/x-www-form-urlencoded; charset=utf-8",
-	       success : function(data) {
-	    	   	var data = data.data;
-	    	   	console.log(data);
-	            var length = data.length;
-	            //每次刷新表中数据,注意给更新结果那一列加上class属性
-	            $('#Ttd').html("");
-	            for(var i=0;i<length;i++)
-	                {
-	                  $('#Ttd').append(
-	                        "<td>"+data[i].tableName+"</td>" +
-	                        "<td>"+data[i].updateDate+"</td>" +
-	                        "<td class=\"updateresult\">"+data[i].result+"</td>");
-	                }
+	// 刷新Ttl数据
+	$.ajax({
+		type : "GET",
+		url : ctx + "getMidDateToTtl",
+		dataType : "json",
+		contentType : "application/x-www-form-urlencoded; charset=utf-8",
+		success : function(data) {
+			var data = data.data;
+			console.log(data);
+			var length = data.length;
+			// 每次刷新表中数据,注意给更新结果那一列加上class属性
+			$('#Ttl').html("");
+			for (var i = 0; i < length; i++) {
+				$('#Ttl').append(
+						"<td>" + data[i].tableName + "</td>" + "<td>"
+								+ data[i].updateDate + "</td>"
+								+ "<td class=\"updateresult\">"
+								+ data[i].result + "</td>");
+			}
+		}
+	});
 
-	       }
-	   }); 
-	   
+	// 刷新Ttd数据
+	$.ajax({
+		type : "GET",
+		url : ctx + "getMidDateToTtd",
+		dataType : "json",
+		contentType : "application/x-www-form-urlencoded; charset=utf-8",
+		success : function(data) {
+			var data = data.data;
+			console.log(data);
+			var length = data.length;
+			// 每次刷新表中数据,注意给更新结果那一列加上class属性
+			$('#Ttd').html("");
+			for (var i = 0; i < length; i++) {
+				$('#Ttd').append(
+						"<td>" + data[i].tableName + "</td>" + "<td>"
+								+ data[i].updateDate + "</td>"
+								+ "<td class=\"updateresult\">"
+								+ data[i].result + "</td>");
+			}
+
+		}
+	});
+	
 }
-
